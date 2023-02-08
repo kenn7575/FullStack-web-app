d086f03da5275ce4b13c570a5dd2f7da0f979733
David Worm Sørensen <davi6583@edu.sde.dk>
2023-02-08 10:50:38 +0100

    V1.2.2

--- DA/ProductRepository.cs
+++ DA/ProductRepository.cs
@@ -5,36 +5,40 @@ using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Reflection.PortableExecutable;
 
 namespace DA
 {
-    internal class ProductRepository : DatabaseAccess
+    public class ProductRepository : DatabaseAccess
     {
         public string Retrieve()
         {
+            var products = new List<Product>();
             // Code that retrieves all products from the database
-            
-            using (SqlConnection connection = new SqlConnection(DatabaseAccess.ConnectionString))
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SqlCommand();
-                command.Connection = connection;
-                command.CommandText = "SELECT * FROM Product";
-                var reader = command.ExecuteReader();
-                var products = new List<Product>();
-                while (reader.Read())
+                string Query = "SELECT * FROM Customer;";
+                using (SqlCommand command = new SqlCommand(Query, connection))
                 {
-                    var product = new Product();
-                    product.ProductID = reader["product_id"] as int?;
-                    product.ProductName = reader["product_name"] as string;
-                    product.Description = reader["description"] as string;
-                    product.CurrentPrice = reader["current_price"] as decimal?;
-                    product.QuantityInStock = reader["quantity"] as int?;
-                    products.Add(product);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var product = new Product();
+                            product.ProductID = reader["product_id"] as int?;
+                            product.ProductName = reader["product_name"] as string;
+                            product.Description = reader["description"] as string;
+                            product.CurrentPrice = reader["current_price"] as decimal?;
+                            product.QuantityInStock = reader["quantity"] as int?;
+                            products.Add(product);
+                        }
+                    }
                 }
-                reader.Close();
-                return JsonSerializer.Serialize(products);
             }
+            return JsonSerializer.Serialize(products);
         }
     }
 }
+
